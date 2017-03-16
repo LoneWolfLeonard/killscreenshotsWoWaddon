bb3c3ab9b041b47f643f7e1a8b51f6419436731c
Leonard Treman <leonardtreman@gmail.com>
2017-03-15 20:49:17 -0400

    Add files via upload
    Internal files of killscreenshots in non zipped format.

---
+++ Frame.lua
@@ -0,0 +1,20 @@
+﻿local SayKill = CreateFrame("Frame")
+
+SayKill:RegisterEvent("PLAYER_PVP_KILLS_CHANGED")
+SayKill:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+SayKill:SetScript("OnEvent", function(Self, event, ...)
+
+if event == "PLAYER_ENTERING_WORLD" then 
+print("Get a kill and KillScreenShots Will Take A Picture of Your Screen.");
+end
+
+if event == "PLAYER_PVP_KILLS_CHANGED" then 
+print("KillScreenshot captured.");
+
+Screenshot() 
+
+
+end
+
+end)
